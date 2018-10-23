095f105431711857c413156057ed2b637200ca46
jim <lj200820082007@163.com>
2018-10-23 21:17:29 +0800

    Site updated: 2018-10-23 21:17:29

--- js/src/love.js
+++ js/src/love.js
@@ -1,3 +1,4 @@
+var clickTimes = 0;
 !function(e,t,a){
     function n(){
         c(".heart{width: 7px;height: 7px;position: fixed;background: #f00;transform: rotate(45deg);-webkit-transform: rotate(45deg);-moz-transform: rotate(45deg);}.heart:after,.heart:before{content: '';width: inherit;height: inherit;background: inherit;border-radius: 50%;-webkit-border-radius: 50%;-moz-border-radius: 50%;position: fixed;}.heart:after{top: -5px;}.heart:before{left: -5px;}"),o(),r()
@@ -7,7 +8,16 @@
             d[e].alpha<=0?(t.body.removeChild(d[e].el),d.splice(e,1)):(d[e].y--,d[e].scale+=.004,d[e].alpha-=.013,d[e].el.style.cssText="left:"+d[e].x+"px;top:"+d[e].y+"px;opacity:"+d[e].alpha+";transform:scale("+d[e].scale+","+d[e].scale+") rotate(45deg);background:"+d[e].color+";z-index:99999");requestAnimationFrame(r)
     }
     function o(){
-        var t="function"==typeof e.onclick&&e.onclick;e.onclick=function(e){t&&t(),i(e)}
+        var t="function"==typeof e.onclick&&e.onclick;
+        e.onclick=function(e){
+            clickTimes = Math.round(Math.random() * 1024);
+            //clickTimes++;
+            if(clickTimes%5 == 0 || clickTimes%7 == 0){
+                clickTimes = 0;
+                t&&t();i(e);
+            }
+        }
+        //e.onclick=function(e){t&&t(),i(e)}
     }
     function i(e){
         var a=t.createElement("div");a.className="heart",d.push({el:a,x:e.clientX-5,y:e.clientY-5,scale:1,alpha:1,color:s()}),t.body.appendChild(a)
